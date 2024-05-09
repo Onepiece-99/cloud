409ce32740194d50dc039308b9056a5d991ef1c6
Onepiece-99 <143369770+Onepiece-99@users.noreply.github.com>
2024-05-09 07:08:40 +0530

    Update client.py

--- prac1/client.py
+++ prac1/client.py
@@ -2,8 +2,11 @@ from zeep import Client
 
 # Create a Zeep client pointing to the SOAP service WSDL
 client = Client('http://localhost:10000/?wsdl')
-
-# Call the add_numbers method of the CalculatorService
-result = client.service.add_numbers(5, 10)
-
-print(f"Result from the SOAP service: {result}")  # Corrected the formatting for displaying the result
+#or client = Client("http://127.0.0.1:7567/?wsdl")
+result_add = client.service.add_numbers(5, 10)
+result_sub = client.service.sub_numbers(5, 10)
+result_mul = client.service.mul_numbers(5, 10)
+print(f"Adding number: {result_add}")
+print(f"Subtracting number: {result_sub}")
+print(f"Multiplying number: {result_mul}")
+#print(f"Result from the SOAP service: {result}") 
