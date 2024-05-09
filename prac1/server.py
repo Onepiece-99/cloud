cc02156a1472f34e2e1c28ba0da3a60526c3c968
Onepiece-99 <143369770+Onepiece-99@users.noreply.github.com>
2024-05-09 07:04:53 +0530

    Update server.py

--- prac1/server.py
+++ prac1/server.py
@@ -4,22 +4,32 @@ from spyne.decorator import rpc
 from spyne.service import ServiceBase
 from spyne.protocol.soap import Soap11
 from spyne.server.wsgi import WsgiApplication
-from spyne.model.primitive import Integer
+from spyne.model.primitive import Integer, Float
 
-class CalculatorService(ServiceBase):
+
+class DivideService(ServiceBase):
+   @rpc(Integer, Integer, _returns=Integer)
+   def mul_numbers(self, num1, num2):
+      return num1 * num2
+
+class CalculationService(ServiceBase):
     @rpc(Integer, Integer, _returns=Integer)
     def add_numbers(self, num1, num2):
         return num1 + num2
+   
+    @rpc(Integer, Integer, _returns=Integer)
+    def sub_numbers(self, num1, num2):
+        return num1 - num2
 
-soap_app = Application([CalculatorService],
+soap_app = Application([CalculationService,DivideService],
                        tns='example.soap',
                        in_protocol=Soap11(validator='lxml'),
                        out_protocol=Soap11())
 
 wsgi_app = WsgiApplication(soap_app)
 
-host = 'localhost'
-port = 10000
+host = 'localhost' #or'127.0.0.1'
+port = 10000 #or'7567'
 
 server = make_server(host, port, wsgi_app)
 print(f"Listening on {host}:{port}")
